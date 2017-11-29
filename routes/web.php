a4862d8f4541a4f61077c75d9de359412072796c
jaymar2595 <jaysonmareno@outlook.com>
2017-11-30 04:16:10 +0800

    Revert "Revert "Lesson 13 Completed""
    This reverts commit ef467a67058f89e34edef408a9739de7bd801f6b.

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,8 @@
 
 Route::get('/', 'PostsController@index');
 Route::get('/posts/create', 'PostsController@create');
-Route::post('/posts/', 'PostsController@store');
-
-// Route::get('/posts/{posts}', 'PostsController@show');
+Route::post('/posts', 'PostsController@store');
+Route::get('/posts/{post}', 'PostsController@show');
 
 
 
